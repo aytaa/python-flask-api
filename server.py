6861edac1f486b85d94bbc4793edeb70c00ddf46
aytaa <Odak1098!&>
2022-08-30 17:15:50 +0300

    mert lale

--- server.py
+++ server.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import os
 
 app = Flask(__name__)
 
+incomes = [
+  { 'name': 'Mert Lale ile Python', 'amount': 5000 , "description" : "Hey gidi python" }
+]
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/books', methods=['GET'])
+def books():
+    return jsonify(incomes)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5005))
